18d2df7948ccfee088c91148acc9649767a1578a
Lucas Sousa <Lucas996oliveira@gmail.com>
2020-08-04 17:00:07 -0300

    Inclusão do modulo de web

---
+++ web/src/pages/Main/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+  PageLanding,
+  PageLandingContent,
+  LogoContainer,
+  HeroImage,
+  ButtonsContainer,
+  TotalConnections,
+} from './styles';
+
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+const Landing: React.FC = () => {
+  return (
+    <PageLanding>
+      <PageLandingContent>
+        <LogoContainer>
+          <img src={logoImg} alt="Proffy" />
+          <h2>Sua plataforma de estudos online</h2>
+        </LogoContainer>
+
+        <HeroImage src={landingImg} alt="Plataforma de estudos" />
+
+        <ButtonsContainer>
+          <Link to="/study" className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </Link>
+          <Link to="/give-classes" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Dar aulas
+          </Link>
+        </ButtonsContainer>
+
+        <TotalConnections>
+          Total de 200 conexões já realizadas
+          <img src={purpleHeartIcon} alt="Coração roxo" />
+        </TotalConnections>
+      </PageLandingContent>
+    </PageLanding>
+  );
+};
+
+export default Landing;
